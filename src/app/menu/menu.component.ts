29b44ebedf4ebb824b4e98d3595f97455909563a
lisacopeland <lisacope@msn.com>
2021-07-06 09:12:02 -0700

    Added styling

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,24 +11,30 @@ export class MenuComponent implements OnInit {
   @Output() endSession = new EventEmitter();
   @Output() menuChoice = new EventEmitter();
   @ViewChild('menubtn', { static: true }) menuBtn: ElementRef;
+  @ViewChild('menulbl', { static: true }) menuLbl: ElementRef;
   @ViewChild('menucard', { static: true }) menucard: ElementRef;
   constructor(private renderer: Renderer2) {
 
     this.renderer.listen('window', 'click', (e: Event) => {
+      console.log('got an event', e);
       const menuOpen = this.menuBtn.nativeElement.checked;
       if (!menuOpen) {
+        console.log('the menu is not open');
         return
       }
       if ((<HTMLElement>e.target).matches('a')) {
           console.log('a choice was made!');
           this.menuBtn.nativeElement.click();
+          return
       }
 
-      if (e.target === this.menuBtn.nativeElement) {
+      if ((e.target === this.menuLbl.nativeElement) || (e.target === this.menuBtn.nativeElement))  {
         // I don't need to do anything, the css will take care of it
         console.log('the target was the button!');
-      }
-
+        // this.menuBtn.nativeElement.click();
+        return;
+      } 
+      
       if (!this.menucard.nativeElement.contains(e.target)) {
         // A click outside the menu, close the menu
         console.log('menu was not clicked!');
